f1231e00050e9add802e7aa2a4fe1855970159c8
Ahmad Karimzade <ahmadkarimzadedev@gmail.com>
2021-03-20 22:13:12 +0330

    feat: console and fonts

---
+++ src/components/SuperConsole.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="super-console">
+		<div class="super-console-header">
+			<span class="super-console__title">SUPER CONSOLE</span>
+			<button class="super-console__close"></button>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'SuperConsole'
+}
+</script>
+
+<style src="@/assets/sass/superConsole.scss" lang="scss" scoped />
